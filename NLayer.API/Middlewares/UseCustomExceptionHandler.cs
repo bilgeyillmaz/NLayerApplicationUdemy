ac89311de59f2a88f0932aab38329c24741c5da7
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2023-01-08 20:15:44 +0300

    Custom Exception Handler created.

---
+++ NLayer.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.DTOs;
+using NLayer.Service.Exceptions;
+
+namespace NLayer.API.Middlewares;
+
+public static class UseCustomExceptionHandler
+{
+    public static void UseCustomException(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(config =>
+        {
+            //run sonlandırıcı bir görev üstlenir bu bölüme gelindiğinde controller a geçilemez.
+            config.Run(async context =>
+            {
+                context.Response.ContentType = "application/json";
+                var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+                var statusCode = exceptionFeature.Error switch
+                {
+                    ClientSideException => 400,
+                    _ => 500
+                };
+
+                context.Response.StatusCode = statusCode;
+                var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+            });
+        });
+    }
+}
